b48fd2624db8703cedc44f7a3c5539d1d2176947
JUMA <jstovar87414@soy.sena.edu.co>
2023-07-24 22:12:07 -0500

    JS primera clase, variables y tipos de datos- formulacion de cuestionario para rellenar una variables

---
+++ js/script.js
@@ -0,0 +1,36 @@
+/*console.log("me la pela");
+alert("ojoooo1");
+
+console.log("2"+"2")
+console.log(2+2)
+console.log(2-2)
+
+//VARIABLES
+
+var edad1 = 30
+let edad2 = 19
+const edad3 = 18
+
+console.log("Edad con varias formas de variables" + edad1, edad2, edad3)
+
+//tipos de datos 
+
+let num = 2.5 
+const name = "Juan"
+const ape = "Tovar"
+
+alert("My name is "+name+" and last name is "+ape)
+
+//para que el programa no se truene cuando es la suma de numeros por variables toca colocar el parseint 
+
+console.log("suma " edad2+num)*/
+
+const name1 = prompt("Escriba su primer nombre: ")
+const name2 = prompt("Escriba su segunco nombre: ")
+const ape1 = prompt("Escriba su primer apellida: ")
+const ape2 = prompt("Escriba su segundo apellido: ")
+const old = prompt("¿Cuantos años tiene?: ")
+const drc = prompt("¿Cual es su dirección acutual?: ")
+const peso = prompt("Cuanto pesa?")
+
+alert("Mi nombre es "+name1+" "+name2+" "+ape1+" "+ape2+", tengo "+old+" años, actualmente peso "+peso+" "+"kg, y estoy viviendo en "+drc)
